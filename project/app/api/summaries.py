dc25ddf2c8ad9d038bb6a317ec21e5c10f7e238e
VargasWagner <wagner.vargasch@gmail.com>
2022-11-07 14:37:48 -0500

    estruct_app_v3

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,11 +1,10 @@
 # project/app/api/summaries.py
- 
- 
 from fastapi import APIRouter, HTTPException
+from typing import List
  
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
- 
+from app.models.tortoise import SummarySchema
  
 router = APIRouter()
  
@@ -18,3 +17,15 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         "url": payload.url
     }
     return response_object
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+   	 raise HTTPException(status_code=404, detail="Summary not found")
+ 
+    return summary
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
